77751794c6828b7a3e993fc19f8f98209d7a949c
Joyce Lin <joycelin79@gmail.com>
2019-05-10 10:18:52 -1000

    add brand

--- src/www/js/header.js
+++ src/www/js/header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import '../css/header.css'
+import project from '../img/project.png'
 
 export default class Header extends Component {
     render() {
         return (
-            <div className="brand-header">
-                <p className="brand">project-log</p>
+            <div className="header">
+                <div className="brand-header">
+                    <img src={project} alt="project-log" className="img-responsive" />
+                    <p className="brand">PROJECT LOG</p>
+                </div>
             </div>
         )
     }
